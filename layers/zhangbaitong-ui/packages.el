12a5b1db85d3ef1382a8a9b81e7772930b431b48
zhangbaitong <zhangbaitong@163.com>
2017-03-22 18:04:02 +0800

    zhangbaitong ui layer

---
+++ layers/zhangbaitong-ui/packages.el
@@ -0,0 +1,17 @@
+;;; packages.el --- zhangbaitong-ui layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: zhangtao <zhangtao@zhangtaodeMacBook-Pro.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+(defconst zhangbaitong-ui-packages
+  '()
+)
+
+;;; packages.el ends here
