1ce5b6d473318dd9af54284c78671a69d1c4dac4
zhangbaitong <zhangbaitong@163.com>
2017-03-26 18:21:07 +0800

    company default ok

--- layers/zhangbaitong-better-defaults/config.el
+++ layers/zhangbaitong-better-defaults/config.el
@@ -1,5 +1,2 @@
-;;; config.el
-(with-eval-after-load 'company
-  (setq company-idle-delay 0.1 )
-  (setq company-minimum-prefix-length 1))
+;; ;;; config.el
 ;;; config.el ends here
--- layers/zhangbaitong-better-defaults/packages.el
+++ layers/zhangbaitong-better-defaults/packages.el
@@ -1,5 +1,13 @@
 ;;; packages.el
 (defconst zhangbaitong-better-defaults-packages
-  '()
+  '(
+    company
+    )
 )
+
+
+(defun zhangbaitong-better-defaults/post-init-company ()
+  (progn
+    (setq company-idle-delay 0.1)
+    (setq company-minimum-prefix-length 1)))
 ;;; packages.el ends here
