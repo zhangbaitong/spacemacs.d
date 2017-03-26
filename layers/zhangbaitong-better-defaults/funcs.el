bb1c49c721a44acbd0e7b6b86c3ad1d121e5433b
zhangbaitong <zhangbaitong@163.com>
2017-03-27 00:59:39 +0800

    better default config for dired

---
+++ layers/zhangbaitong-better-defaults/funcs.el
@@ -0,0 +1,8 @@
+;;; funcs.el
+
+(defun zbt/dired-up-directory()
+  "goto up directory and resue buffer"
+  (interactive)
+  (find-alternate-file ".."))
+
+;;; funcs.el end here
--- layers/zhangbaitong-better-defaults/packages.el
+++ layers/zhangbaitong-better-defaults/packages.el
@@ -2,12 +2,24 @@
 (defconst zhangbaitong-better-defaults-packages
   '(
     company
+    (dired-mode :location built-in)
     )
 )
 
 
 (defun zhangbaitong-better-defaults/post-init-company ()
   (progn
-    (setq company-idle-delay 0.1)
+    (setq company-idle-delay 0.08)
     (setq company-minimum-prefix-length 1)))
-;;; packages.el ends here
+
+(defun zhangbaitong-better-defaults/init-dired-mode()
+  (use-package dired-mode
+    :defer t
+    :init
+    (progn
+      (add-hook 'dired-mode-hook (lambda ()
+                                   (define-key dired-mode-map (kbd "DEL")  'zbt/dired-up-directory)))
+      (add-hook 'dired-mode-hook (lambda ()
+                                   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)))
+      )))
+;;; packages.el ends here 
