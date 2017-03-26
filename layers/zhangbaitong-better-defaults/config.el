2e0808691acfe04afc6f4f21a80eb88f2115a624
zhangbaitong <zhangbaitong@163.com>
2017-03-26 13:15:29 +0800

    config default commpany variable

---
+++ layers/zhangbaitong-better-defaults/config.el
@@ -0,0 +1,7 @@
+;;; config.el
+
+(with-eval-after-load 'company
+  (setq company-idle-delay 0.1 )
+  (setq company-minimum-prefix-length 1))
+
+;;; config.el ends here
--- layers/zhangbaitong-better-defaults/packages.el
+++ layers/zhangbaitong-better-defaults/packages.el
@@ -1,62 +1,7 @@
-;;; packages.el --- zhangbaitong-better-defaults layer packages file for Spacemacs.
-;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
-;;
-;; Author: zhangtao <zhangtao@zhangtaodeMacBook-Pro.local>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
-
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `zhangbaitong-better-defaults-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `zhangbaitong-better-defaults/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `zhangbaitong-better-defaults/pre-init-PACKAGE' and/or
-;;   `zhangbaitong-better-defaults/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
+;;; packages.el
 (defconst zhangbaitong-better-defaults-packages
   '()
-  "The list of Lisp packages required by the zhangbaitong-better-defaults layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+)
 
 
 ;;; packages.el ends here
